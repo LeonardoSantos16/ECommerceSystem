fa9e02734c7f86bc625b8f2eff5b7afe9f548976
leonardo ferreira <leodsmf@gmail.com>
2026-05-20 21:12:40 -0300

    added Kafka and MassTransit producer

---
+++ ECommerce.Checkout.API/DTOs/Contracts.cs
@@ -0,0 +1,14 @@
+﻿namespace ECommerce.Checkout.API.DTOs
+{
+    public class Contracts
+    {
+        public record Order(string Id, string CustomerId, decimal TotalAmount);
+
+        public record OrderCreatedEvent(
+            string OrderId,
+            string CustomerId,
+            decimal TotalAmount,
+            DateTime CreatedAt
+        );
+    }
+}
--- ECommerce.Checkout.API/Program.cs
+++ ECommerce.Checkout.API/Program.cs
@@ -1,4 +1,6 @@
+using MassTransit;
 using MongoDB.Driver;
+using static ECommerce.Checkout.API.DTOs.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,24 @@ builder.Services.AddSingleton(database);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingInMemory((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+
+    x.AddRider(rider =>
+    {
+        rider.AddProducer<OrderCreatedEvent>("order-created-topic");
+
+        rider.UsingKafka((context, k) =>
+        {
+            k.Host("localhost:9092"); 
+        });
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -18,13 +38,24 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.MapPost("/api/checkout/orders", async (Order order, IMongoDatabase db) =>
+app.MapPost("/api/checkout/orders", async (
+    Order order,
+    IMongoDatabase db,
+    ITopicProducer<OrderCreatedEvent> producer) =>
 {
     var collection = db.GetCollection<Order>("orders");
+
     await collection.InsertOneAsync(order);
+
+    await producer.Produce(new OrderCreatedEvent(
+        order.Id,
+        order.CustomerId,
+        order.TotalAmount,
+        DateTime.UtcNow
+    ));
+
     return Results.Accepted($"/api/checkout/orders/{order.Id}", order);
 });
 
 app.Run();
 
-public record Order(string Id, string CustomerId, decimal TotalAmount);
